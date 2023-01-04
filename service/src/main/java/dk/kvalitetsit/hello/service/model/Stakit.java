247323cbc702ba3124ed065119185662ca099d4c
Amalie Dobel Christiansen <adc@kvalitetsit.dk>
2023-01-04 14:23:33 +0100

    Yaml parse

---
+++ service/src/main/java/dk/kvalitetsit/hello/service/model/Stakit.java
@@ -0,0 +1,23 @@
+package dk.kvalitetsit.hello.service.model;
+
+public class Stakit {
+    private String endpoint;
+
+    private String apiKey;
+
+    public String getEndpoint() {
+        return endpoint;
+    }
+
+    public void setEndpoint(String endpoint) {
+        this.endpoint = endpoint;
+    }
+
+    public String getApiKey() {
+        return apiKey;
+    }
+
+    public void setApiKey(String apiKey) {
+        this.apiKey = apiKey;
+    }
+}
---
+++ service/src/test/java/dk/kvalitetsit/hello/service/ConfigurationReaderTest.java
@@ -0,0 +1,41 @@
+package dk.kvalitetsit.hello.service;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.FileNotFoundException;
+import java.nio.file.Paths;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class ConfigurationReaderTest {
+
+    private ConfigurationReader configurationReader;
+
+    @Before
+    public void setup() {
+        configurationReader = new ConfigurationReader();
+    }
+
+    @Test
+    public void monitorTest() throws FileNotFoundException {
+        //System.out.println(Paths.get(".").toAbsolutePath());
+
+        var input = "src/test/resources/TestConfiguration.yaml";
+
+        var result = configurationReader.readConfiguration(input);
+        assertNotNull(result);
+        assertNotNull(result.getStakit());
+        assertEquals("https://backend.dk/", result.getStakit().getEndpoint());
+        assertEquals("MY-SUPE-SECRET-KEY", result.getStakit().getApiKey());
+
+        assertEquals("PT5M", result.getFrequency());
+
+        assertNotNull(result.getMonitoring());
+        assertEquals(2, result.getMonitoring().size());
+        assertEquals("unikt id", result.getMonitoring().get(0).getServiceIdentifier());
+        assertEquals("Noget andet navn", result.getMonitoring().get(1).getServiceName());
+        assertEquals("http://en anden service", result.getMonitoring().get(1).getEndpoint());
+    }
+}
